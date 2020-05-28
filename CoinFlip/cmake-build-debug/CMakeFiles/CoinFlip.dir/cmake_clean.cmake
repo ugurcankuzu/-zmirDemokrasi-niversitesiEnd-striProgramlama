2a6366c755ca7e71af332e833c4c185ac7491c1f
ugurcankuzu <58211421+ugurcankuzu@users.noreply.github.com>
2020-05-29 02:23:08 +0300

    Add files via upload

---
+++ CoinFlip/cmake-build-debug/CMakeFiles/CoinFlip.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/CoinFlip.dir/main.cpp.o"
+  "CoinFlip"
+  "CoinFlip.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/CoinFlip.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ CoinFlip/main.cpp
@@ -0,0 +1,61 @@
+#include<iostream>
+#include<ctime>
+#include<cstdlib>
+#include<cmath>
+#include <locale.h>
+
+using namespace std;
+
+int main(){
+    setlocale(LC_ALL,"Turkish");
+    cout << "**************************************************\n"
+            "#######        YAZI-TURA SİMÜLASYONU       #######\n"
+            "**************************************************\n\n";
+
+
+
+    int yazıSayısı = 0;
+    int turaSayısı = 0;
+    int atış=0;
+    char a;
+    cout << "Kaç kere yazı-tura atmak istiyorsunuz?";
+    cin >> atış;
+
+    srand(time(0));
+
+
+    while (atış > 0){
+            a = (rand()%2)+1;
+
+            if (a == 1){
+                cout << "Yazı"<< endl;
+                yazıSayısı ++;
+            }
+            else if (a == 2){
+                cout << "Tura"<< endl;
+                turaSayısı ++;
+            }
+            atış --;
+
+
+    }
+
+    if(atış == 0){
+        cout << "Gelen yazı sayısı:" << yazıSayısı << endl;
+        cout << "Gelen tura sayısı:" << turaSayısı;
+    }
+
+}
+int flip()
+{
+    setlocale(LC_ALL,"Turkish");
+    int c_num;
+    c_num= (rand()%2)+1;
+
+    return c_num;
+}
+
+
+
+
+
